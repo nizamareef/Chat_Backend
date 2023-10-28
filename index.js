ed155d5b7cc1627a1f8dc5d78c1002c097c84f7d
areef <nizamareef@gmail.com>
2023-10-29 00:10:21 +0530

    first commit

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ app.use("/api/messages", require("./Routes/messages"))
 
 
 //---------------------Deployment-----------------------
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  );
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+//   );
 
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 //---------------------Deployment-----------------------
 
 const server = app.listen(port, console.log(`Server listening at ${port}`))
